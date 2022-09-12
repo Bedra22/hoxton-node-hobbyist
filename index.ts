aafdbea5adb6c745451fca703df4c6cbe581fd82
Bedra Kraja <bedra.kraja22@gmail.com>
2022-09-12 19:31:46 +0200

    Finished

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ app.use(express.json())
 app.use(cors())
 const prisma = new PrismaClient({ log: ['warn', 'error', 'info', 'query'] })
 
+//users CRUD
 
 app.get('/users', async (req, res) => {
     const user = await prisma.users.findMany({ include: { Hoobies: true } })
@@ -19,6 +20,20 @@ app.post('/users', async (req, res) => {
     res.send(newUser)
 })
 
+app.patch('/users/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const updateUser = await prisma.users.update({ data: req.body, where: { id }, include: { Hoobies: true } })
+    res.send(updateUser)
+})
+
+app.delete('/users/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const deleteUser = await prisma.users.delete({ where: { id } })
+    res.send(deleteUser)
+})
+
+//hobbies CRUD
+
 app.get('/hoobies', async (req, res) => {
     const hoobies = await prisma.hoobies.findMany({ include: { user: true } })
     res.send(hoobies)
@@ -29,6 +44,17 @@ app.post('/hoobies', async (req, res) => {
     res.send(newHooby)
 })
 
+app.patch('/hoobies/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const updateHobbies = await prisma.hoobies.update({ data: req.body, where: { id }, include: { user: true } })
+    res.send(updateHobbies)
+})
+
+app.delete('/hoobies/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const deleteHobby = await prisma.hoobies.delete({ where: { id } })
+    res.send(deleteHobby)
+})
 
 app.listen(port, () => {
     console.log(`We are running on http://localhost:${port}`)
